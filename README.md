2ada120ae3d0ee29cb9f798af23fd08fd665947d
Wilko Nienhaus <wilko.nienhaus@gmail.com>
2021-11-13 16:30:22 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# ujqtvms
+
+A simple project to test publising to pypi using Github Actions, when
+creating a Release using Github.
+
+(The name ujqtvms was chosen at random in attempt to find something
+that wouldn't ever be used - thus not cluttering up pypi's namespace,
+given that modules by a given name can only ever be created once)
---
+++ setup.py
@@ -0,0 +1,34 @@
+import re
+import sdist_upip
+from setuptools import setup
+
+VERSION = "1.0.0"
+
+
+def long_desc_from_readme():
+    with open('README.md', 'r') as fd:
+        long_description = fd.read()
+
+        return long_description
+
+
+setup(
+    name="ujqtvms",
+    version=VERSION,
+    description="Testing automated pypi publishing vi Github releases",
+    long_description=long_desc_from_readme(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/wnienhaus/ujqtvms",
+    license="MIT",
+    author="ujqtvms authors",
+    author_email="wnienhaus@email.com",
+    maintainer="ujqtvms authors",
+    maintainer_email="wnienhaus@email.com",
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: Implementation :: MicroPython',
+    ],
+    platforms=["esp32", "linux", "darwin"],
+    cmdclass={"sdist": sdist_upip.sdist},
+    packages=["ujqtvms"],
+)
